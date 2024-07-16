392defdcaf22453b92fe85d0e99b638ea52a4031
Killerpug <etapia.clr@outlook.com>
2024-07-15 21:29:43 -0600

    encrypt challenge font end

--- Act03_EncryptChallenge/app.js
+++ Act03_EncryptChallenge/app.js
@@ -4,10 +4,6 @@ let letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
     'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
     'Y', 'Z' ]
 //let row = document.querySelector('#row1').querySelectorAll();
-let textarea = document.getElementById("test-target");
-textarea.addEventListener("keyup", (e) => {
-    console.log(`Key "${e.key}" released [event: keyup]`);
-  });
 
 
 
