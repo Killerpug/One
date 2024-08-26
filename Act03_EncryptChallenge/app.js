ecedaa27e6f0abf2ba03b5f58122c81e4063d607
Killerpug <etapia.clr@outlook.com>
2024-08-26 16:18:28 -0600

    complete encrypt challenge

--- Act03_EncryptChallenge/app.js
+++ Act03_EncryptChallenge/app.js
@@ -1,15 +1,42 @@
 
-// Initial conditions
-let letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z' ]
-//let row = document.querySelector('#row1').querySelectorAll();
+function Encrypt() {
+    var startTime = performance.now()
+    let inputText = document.getElementById('sourceText').value;
 
+    const replacements = {
+        'e': 'enter',
+        'i': 'imes',
+        'a': 'ai',
+        'o': 'ober',
+        'u': 'ufat'
+    };
+    let encryptedText = inputText.replace(/[eioua]/g, function(match) {
+        return replacements[match];
+    });
+    
+    document.querySelector('#targetText').value = encryptedText;
+    var endTime = performance.now()
+    console.log(`Call to Encrypt took ${endTime - startTime} milliseconds`)
+}
 
+function Decrypt() {
+    var startTime = performance.now()
+    let inputText = document.getElementById('targetText').value;
 
-function InitialConditions() {
-
-
+    const reverseReplacements = {
+        'enter': 'e',
+        'imes': 'i',
+        'ai': 'a',
+        'ober': 'o',
+        'ufat': 'u'
+    };
+    let decryptedText = inputText.replace(/enter|imes|ai|ober|ufat/g, function(match) {
+        return reverseReplacements[match];
+    });
+    
+    document.querySelector('#sourceText').value = decryptedText;
+    var endTime = performance.now()
+    console.log(`Call to Decrypt took ${endTime - startTime} milliseconds`)
 }
 
 InitialConditions();
